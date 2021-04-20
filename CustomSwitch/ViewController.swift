6ede03294be39b35927780b322a425db55425c37
y-samrith <y-samrith@beniten.com>
2021-04-20 08:53:10 +0700

    Initial Commit

---
+++ CustomSwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSwitch
+//
+//  Created by Samrith Yoeun on 20/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
